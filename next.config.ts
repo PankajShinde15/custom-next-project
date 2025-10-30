85e7bf1eae2d954747e4e57811fc790668acd1dc
Your Name <you@example.com>
2025-10-30 20:53:50 +0530

    Added next.config file

---
+++ next.config.ts
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  basePath: "/custom-next-project",
+  assetPrefix: "/custom-next-project/",
+};
+
+export default nextConfig;
