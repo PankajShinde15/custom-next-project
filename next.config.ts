42346b3f187eff86f7ad8fa3d971ef6b90813d9b
Your Name <you@example.com>
2025-10-30 20:59:00 +0530

    deleted ts file

--- next.config.ts
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  basePath: "/custom-next-project",
-  assetPrefix: "/custom-next-project/",
-};
-
-export default nextConfig;
